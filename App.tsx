d50b3503949d0b714a54f0448478f28d6c408bd9
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2025-02-23 18:04:46 +0600

    some component testing

--- App.tsx
+++ App.tsx
@@ -5,13 +5,18 @@
  * @format
  */
 
-import React from 'react';
 import type {PropsWithChildren} from 'react';
+import React from 'react';
 import {
+  Alert,
+  Button,
+  Image,
+  Pressable,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
+  TouchableOpacity,
   useColorScheme,
   View,
 } from 'react-native';
@@ -78,10 +83,35 @@ function App(): React.JSX.Element {
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        style={backgroundStyle}>
+      <ScrollView style={backgroundStyle}>
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+            paddingHorizontal: safePadding,
+            paddingBottom: safePadding,
+          }}>
+          <Text>Hello</Text>
+          <Image
+            style={styles.image}
+            source={{
+              uri: 'https://scontent.fdac14-1.fna.fbcdn.net/v/t39.30808-6/480879851_10015089978518594_6975596949791265783_n.jpg?stp=cp6_dst-jpegr_tt6&_nc_cat=110&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeFq8GgT1dwgR5l_VXyhczDBwohhwVfKZtbCiGHBV8pm1m8Esc8AwUUoLNv85eqjESLVIVEmXwhGTwLYL3gkDW2v&_nc_ohc=q_iSl6rbMnYQ7kNvgEIT1Ns&_nc_oc=AdhVpEAlGetYRM7jzN3c5dv_qcbOn0uZNiukaRMISXY0EIp4FEjXQdVQjZIhvdz-wmA&_nc_zt=23&se=-1&_nc_ht=scontent.fdac14-1.fna&_nc_gid=AbDMAszI3cum_m2C6Nlgpfq&oh=00_AYB7xCUc6elaINYqFx6d6uW9_KbuIVV3e_qu_XpOGO_XGg&oe=67C0CAE7',
+            }}
+          />
+          <Button title="It jus a Static Button" />
+          <TouchableOpacity
+            // eslint-disable-next-line react-native/no-inline-styles
+            style={{backgroundColor: 'green', padding: 10, borderRadius: 5}}
+            onPress={() => {
+              Alert.alert('Hello');
+            }} >
+            <Text>It's Touchable Opacity button</Text>
+          </TouchableOpacity>
+          <Pressable style={{padding: 10, backgroundColor: 'yellow'}}>
+            <Text>Pressable button</Text>
+          </Pressable>
+        </View>
         <View style={{paddingRight: safePadding}}>
-          <Header/>
+          <Header />
         </View>
         <View
           style={{
@@ -125,6 +155,10 @@ const styles = StyleSheet.create({
   },
   highlight: {
     fontWeight: '700',
+  },
+  image: {
+    width: 200,
+    height: 200,
   },
 });
 
